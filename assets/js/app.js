5a5c74535f9c9e7d26a7b61d08217f79d449b079
David Aji <davidenochaji@gmail.com>
2021-05-17 23:14:29 +0100

    fix:made changes to UI

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,40 @@ window.onload = function () {
 	};
 	document.querySelector(".bars").addEventListener("click", toggleNav);
 
+	  // show timestamp in readable format
+  // Epochs
+  const epochs = [
+    ['year', 31536000],
+    ['month', 2592000],
+    ['day', 86400],
+    ['hour', 3600],
+    ['minute', 60],
+    ['second', 1],
+  ];
+
+  // Get duration
+  const getDuration = (timeAgoInSeconds) => {
+    for (let [name, seconds] of epochs) {
+      const interval = Math.floor(timeAgoInSeconds / seconds);
+
+      if (interval >= 1) {
+        return {
+          interval: interval,
+          epoch: name,
+        };
+      }
+    }
+  };
+
+  // Calculate
+  const timeAgo = (date) => {
+    const timeAgoInSeconds = Math.floor((new Date() - new Date(date)) / 1000);
+    const { interval, epoch } = getDuration(timeAgoInSeconds);
+    const suffix = interval === 1 ? '' : 's';
+
+    return `${interval} ${epoch}${suffix} ago`;
+  };
+
 	const getData = async () => {
 		let res = await fetch("https://david-github-clone.netlify.app/.netlify/functions/githubclone", {
 			method: "GET",
@@ -16,46 +50,6 @@ window.onload = function () {
 		let data = await res.json();
 		injectData(data.data.viewer);
 	};
-
-	// const getData = async () => {
-	// 	let GITHUB_TOKEN = await getToken();
-	// 	let res = await fetch("https://api.github.com/graphql", {
-	// 		method: "POST",
-	// 		headers: {
-	// 			Authorization: `bearer ${GITHUB_TOKEN}`,
-	// 		},
-	// 		body: JSON.stringify({
-	// 			query: `{
-	// 				viewer {
-	// 					login
-	// 					repositories(last: 20, ownerAffiliations: OWNER) {
-	// 						edges {
-	// 							node {
-	// 								id
-	// 								url
-	// 								updatedAt
-	// 								primaryLanguage {
-	// 									color
-	// 									name
-	// 								}
-	// 								name
-	// 								description
-	// 								forkCount
-	// 								isPrivate
-	// 								stargazerCount
-	// 							}
-	// 						}
-	// 					}
-	// 					avatarUrl
-	// 					bio
-	// 					name
-	// 				}
-	// 			}`,
-	// 		}),
-	// 	});
-	// 	let data = await res.json();
-	// 	injectData(data.data.viewer);
-	// };
 
 	const injectData = (data) => {
 		// add avatar images
